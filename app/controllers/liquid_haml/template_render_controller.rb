98fe2afe6627e6261cdd7a4edbfe377b74c35719
Tomek Mańko <jaennirin@gmail.com>
2013-08-27 16:48:42 +0200

    minor changes

--- app/controllers/liquid_haml/template_render_controller.rb
+++ app/controllers/liquid_haml/template_render_controller.rb
@@ -30,13 +30,25 @@ class LiquidHaml::TemplateRenderController < ApplicationController
 
     # end
 
+    def url_options
+      options = super
+      binding.pry
+      options
+    end
+
     def self.view_context_class
       context_class = super
       context_class.send(:include, Rails.application.routes.url_helpers)
+      binding.pry
+      context_class
     end
 
     def _render_template(options)
       lookup_context.rendered_format = nil if options[:formats]
+
+      a = view_context
+      binding.pry
+      path = a.items_path
 
       view_renderer.render(view_context, options)
     end
